5610ae077352b5d296f4e7d312d0332fc53c3f3e
gabri <gabrielmark.a.capalad@gmail.com>
2024-12-12 11:17:21 +0800

    small changes

--- src/components/lineupMinds.vue
+++ src/components/lineupMinds.vue
@@ -1,106 +1,132 @@
 <template>
-    <div class="lineupMinds">
-        <div class="invisBox">
-            <div class="globalHeader">
-                <h1>Minds Behind </h1>
-                <img :src="lineupLogo" class="logo">
-            </div>
-            <div class="container">
-                <div class="dev" v-for="(dev, index) in devs" :key="index">
-                    <img :src="dev.img" alt="devbabyface">
-                    <div class="devInfo">
-                        <p class="name"> {{ dev.name }} </p>
-                        <p class="position"> {{ dev.position }} </p>
-                    </div>
-                    <div class="socIcons">
-                        <img :src="gitLogo" class="git" @click="redirectTo(dev.github)">
-                        <img :src="linkedLogo" class="linked" @click="redirectTo(dev.linkedin)">
-                    </div>
-                </div>
-            </div>
+  <div class="lineupMinds">
+    <div class="invisBox">
+      <div class="globalHeader">
+        <h1>Minds Behind</h1>
+        <img :src="lineupLogo" class="logo" />
+      </div>
+      <div class="container">
+        <div class="dev" v-for="(dev, index) in devs" :key="index">
+          <img :src="dev.img" alt="devbabyface" />
+          <div class="devInfo">
+            <p class="name">{{ dev.name }}</p>
+            <p class="position">{{ dev.position }}</p>
+          </div>
+          <div class="socIcons">
+            <img :src="gitLogo" class="git" @click="redirectTo(dev.github)" />
+            <img
+              :src="linkedLogo"
+              class="linked"
+              @click="redirectTo(dev.linkedin)"
+            />
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import lineupLogo from '@/assets/lineupLogo.svg'
-import gitLogo from '@/assets/gitLogo.svg'
-import linkedLogo from '@/assets/linkedLogo.svg'
-import nicky from '@/assets/nicky.svg'
-import gem from '@/assets/gem.svg'
-import alvir from '@/assets/alvir.svg'
+import lineupLogo from "@/assets/lineupLogo.svg";
+import gitLogo from "@/assets/gitLogo.svg";
+import linkedLogo from "@/assets/linkedLogo.svg";
+import nicky from "@/assets/nicky.svg";
+import gem from "@/assets/gem.svg";
+import alvir from "@/assets/alvir.svg";
 
 export default {
-    name: "lineupMinds",
-    data() {
-        return {
-            lineupLogo,
-            gitLogo,
-            linkedLogo,
-            nicky,
-            gem,
-            alvir,
-            circles: Array(3).fill(null),
-            devs: [
-                { name: "Nicky Ronald Y. Cadalig Jr.", position: "Frontend Developer", github: "https://github.com/levaaaay", linkedin: "https://github.com/levaaaay", img: nicky},
-                { name: "Gabriel Mark A. Capalad", position: "UI/UX Designer", github: "https://github.com/gemcapalad", linkedin: "https://github.com/levaaaay", img: gem},
-                { name: "Peter Alvir M. Gonzales", position: "Backend Developer", github: "https://github.com/peteralvir", linkedin: "https://github.com/peteralvir", img: alvir}
-            ]
-        }
+  name: "lineupMinds",
+  data() {
+    return {
+      lineupLogo,
+      gitLogo,
+      linkedLogo,
+      nicky,
+      gem,
+      alvir,
+      circles: Array(3).fill(null),
+      devs: [
+        {
+          name: "Nicky Ronald Y. Cadalig Jr.",
+          position: "Frontend Developer",
+          github: "https://github.com/levaaaay",
+          linkedin: "https://github.com/levaaaay",
+          img: nicky,
+        },
+        {
+          name: "Gabriel Mark A. Capalad",
+          position: "UI/UX Designer",
+          github: "https://github.com/gemcapalad",
+          linkedin: "https://github.com/levaaaay",
+          img: gem,
+        },
+        {
+          name: "Peter Alvir M. Gonzales",
+          position: "Backend Developer",
+          github: "https://github.com/peteralvir",
+          linkedin: "https://github.com/peteralvir",
+          img: alvir,
+        },
+      ],
+    };
+  },
+  methods: {
+    redirectTo(url) {
+      window.open(url, "_blank");
     },
-    methods: {
-        redirectTo(url) {
-            window.open(url, "_blank");
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
 .lineupMinds {
-   background: #E9ECEF;
-   height: 41rem;
+  background: #e9ecef;
+  height: 41rem;
 }
 
 .logo {
-    width: 8.31rem;
-    height: 4.75rem; 
-    margin: 3.3rem 0.5rem 0 1rem;
+  width: 8.31rem;
+  height: 4.75rem;
+  margin: 3.3rem 0.5rem 0 1rem;
 }
 
 .container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-evenly;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
 }
 
 .devContainer {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .minds {
-    width: 17.7475rem;
-    height: 17.73rem;
-    border-radius: 50%;
-    background-color: #E0E4EB;
-    border: 2px solid #E0E4EB;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.3);
+  width: 17.7475rem;
+  height: 17.73rem;
+  border-radius: 50%;
+  background-color: #e0e4eb;
+  border: 2px solid #e0e4eb;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.3);
 }
 
 .devInfo {
-    text-align: center;
-    margin-top: 1.25rem;
+  text-align: center;
+  margin-top: 1.25rem;
 }
 
 .name {
-    font-size: 1.5rem;
-    font-weight: bold;
-    color: #212631;
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #212631;
+}
+
+.position {
+  text-align: center;
 }
 
 .socIcons {
@@ -109,12 +135,12 @@ export default {
 }
 
 .git {
-    width: 3rem;
-    height: 3rem;
+  width: 3rem;
+  height: 3rem;
 }
 
 .linked {
-    width: 3rem;
-    height: 3rem;
+  width: 3rem;
+  height: 3rem;
 }
 </style>
--- src/components/staff/stafflandpage.vue
+++ src/components/staff/stafflandpage.vue
@@ -23,178 +23,178 @@
 </template>
 
 <script>
-  import ltoLogo from "@/assets/ltoLogo.svg";
-  import dfaLogo from "@/assets/dfaLogo.svg";
-  import prcLogo from "@/assets/prcLogo.svg";
-  import birLogo from "@/assets/birLogo.svg";
-  import psaLogo from "@/assets/psaLogo.svg";
-  import lineupLogo from "@/assets/lineupLogo.svg";
-  import { supabase } from "@/client/supabase";
+import ltoLogo from "@/assets/ltoLogo.svg";
+import dfaLogo from "@/assets/dfaLogo.svg";
+import prcLogo from "@/assets/prcLogo.svg";
+import birLogo from "@/assets/birLogo.svg";
+import psaLogo from "@/assets/psaLogo.svg";
+import lineupLogo from "@/assets/lineupLogo.svg";
+import { supabase } from "@/client/supabase";
 
-  export default {
-    name: "landPage",
-    data() {
-      return {
-        name: null,
-        ltoLogo,
-        dfaLogo,
-        prcLogo,
-        birLogo,
-        psaLogo,
-        lineupLogo,
-        queueText: "in queue today.",
-        queueNumber: null,
-      };
+export default {
+  name: "landPage",
+  data() {
+    return {
+      name: null,
+      ltoLogo,
+      dfaLogo,
+      prcLogo,
+      birLogo,
+      psaLogo,
+      lineupLogo,
+      queueText: "in queue today.",
+      queueNumber: null,
+    };
+  },
+  mounted() {
+    this.getTotalQueueNumber();
+    this.displayName();
+  },
+  methods: {
+    queue() {
+      this.$router.push({ name: "services" }).then(() => {});
     },
-    mounted() {
-      this.getTotalQueueNumber();
-      this.displayName();
-    },
-    methods: {
-      queue() {
-        this.$router.push({ name: "services" }).then(() => {});
-      },
-      async getTotalQueueNumber() {
-        const today = new Date(
-          new Date().toLocaleString("en-US", { timeZone: "Asia/Manila" })
-        );
-        const currentDay = new Date(today);
-        const dateString = currentDay.toLocaleDateString("en-CA");
-        const { count, error } = await supabase
-          .from("tickets")
-          .select("ticket_id", { count: "exact", head: true })
-          .eq("queue_date", dateString);
+    async getTotalQueueNumber() {
+      const today = new Date(
+        new Date().toLocaleString("en-US", { timeZone: "Asia/Manila" })
+      );
+      const currentDay = new Date(today);
+      const dateString = currentDay.toLocaleDateString("en-CA");
+      const { count, error } = await supabase
+        .from("tickets")
+        .select("ticket_id", { count: "exact", head: true })
+        .eq("queue_date", dateString);
 
-        this.queueNumber = count;
-      },
-      async displayName() {
-        const {
-          data: { session },
-          error,
-        } = await supabase.auth.getSession();
-
-        if (session) {
-          const { data, error } = await supabase
-            .from("users")
-            .select("display_name")
-            .eq("email", session.user.email);
-            console.log(session.user.email)
-          this.name = data[0].display_name;
-        }
-      },
+      this.queueNumber = count;
     },
-  };
+    async displayName() {
+      const {
+        data: { session },
+        error,
+      } = await supabase.auth.getSession();
+
+      if (session) {
+        const { data, error } = await supabase
+          .from("users")
+          .select("display_name")
+          .eq("email", session.user.email);
+        console.log(session.user.email);
+        this.name = data[0].display_name;
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .landPage {
-    background: #dee2e6;
-    height: 39.3rem;
-  }
+.landPage {
+  background: #dee2e6;
+  height: 39.3rem;
+}
 
-  h1 {
-    font-weight: 800;
-    padding-top: 1rem;
-    padding-bottom: 2rem;
-  }
+h1 {
+  font-weight: 800;
+  padding-top: 1rem;
+  padding-bottom: 2rem;
+}
 
-  .logo {
-    width: 8.3rem;
-    height: 4.75rem;
-    margin: -0.6rem 0.1rem 0 1rem;
-  }
+.logo {
+  width: 8.3rem;
+  height: 4.75rem;
+  margin: -0.6rem 0.1rem 0 1rem;
+}
 
-  @keyframes slide {
-    from {
-      transform: translateX(0);
-    }
-    to {
-      transform: translate(-100%);
-    }
+@keyframes slide {
+  from {
+    transform: translateX(0);
   }
+  to {
+    transform: translate(-100%);
+  }
+}
 
-  .logos {
-    overflow: hidden;
-    padding: 2.5rem;
-    white-space: nowrap;
-    position: relative;
-  }
+.logos {
+  overflow: hidden;
+  padding: 2.5rem;
+  white-space: nowrap;
+  position: relative;
+}
 
-  .logos:before,
-  .logos:after {
-    position: absolute;
-    top: 0;
-    width: 15.625rem;
-    height: 100%;
-    content: "";
-    z-index: 2;
-  }
+.logos:before,
+.logos:after {
+  position: absolute;
+  top: 0;
+  width: 15.625rem;
+  height: 100%;
+  content: "";
+  z-index: 2;
+}
 
-  .logos:before {
-    left: 0;
-    background: linear-gradient(to left, rgba(255, 255, 255, 0), #dee2e6);
-  }
+.logos:before {
+  left: 0;
+  background: linear-gradient(to left, rgba(255, 255, 255, 0), #dee2e6);
+}
 
-  .logos:after {
-    right: 0;
-    background: linear-gradient(to right, rgba(255, 255, 255, 0), #dee2e6);
-  }
+.logos:after {
+  right: 0;
+  background: linear-gradient(to right, rgba(255, 255, 255, 0), #dee2e6);
+}
 
-  .logos:hover .logosSlide {
-    animation-play-state: paused;
-  }
+.logos:hover .logosSlide {
+  animation-play-state: paused;
+}
 
-  .logosSlide {
-    display: inline-block;
-    animation: 25s slide infinite linear;
-  }
+.logosSlide {
+  display: inline-block;
+  animation: 25s slide infinite linear;
+}
 
-  .logosSlide img {
-    height: 18rem;
-    margin: -2.5rem 0 -2.5rem 2.5rem;
-  }
+.logosSlide img {
+  height: 18rem;
+  margin: -2.5rem 0 -2.5rem 2.5rem;
+}
 
-  .comingSoon {
-    opacity: 0.5;
-  }
+.comingSoon {
+  opacity: 0.5;
+}
 
-  .queueText {
-    display: flex;
-    justify-content: center;
-    font-size: 1.25rem;
-    align-items: center;
-    margin-top: 3rem;
-    font-weight: 700;
-  }
+.queueText {
+  display: flex;
+  justify-content: center;
+  font-size: 1.25rem;
+  align-items: center;
+  margin-top: 3rem;
+  font-weight: 700;
+}
 
-  .dynamic {
-    color: #2252a0;
-    margin-right: 0.625rem;
-  }
+.dynamic {
+  color: #2252a0;
+  margin-right: 0.625rem;
+}
 
-  .btn {
-    width: Fill (8.625rem);
-    height: Hug 3.125rem;
-    padding: 1rem 1.5rem 1rem 1.5rem;
-    gap: 0.5rem;
-    border: none;
-    background: var(--colors-blue-blue-800, #052c65);
-    font-weight: 600;
-    font-size: 1rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.btn {
+  width: Fill (8.625rem);
+  height: Hug 3.125rem;
+  padding: 1rem 1.5rem 1rem 1.5rem;
+  gap: 0.5rem;
+  border: none;
+  background: var(--colors-blue-blue-800, #052c65);
+  font-weight: 600;
+  font-size: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-  .btn:hover {
-    color: #f0f1f5;
-    background-color: #084298;
-  }
+.btn:hover {
+  color: #f0f1f5;
+  background-color: #084298;
+}
 
-  .queueNowButton {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 0.93rem;
-  }
+.queueNowButton {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 0.93rem;
+}
 </style>
