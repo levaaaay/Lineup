f4a746dd4ad4d504e109ed9c20558204f79665f4
Your Name <ncadaligjr@gmail.com>
2025-01-20 10:11:58 +0800

    q

--- src/components/qWithL.vue
+++ src/components/qWithL.vue
@@ -9,16 +9,40 @@
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7" aria-label="Slide 8"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8" aria-label="Slide 9"></button>
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img :src="img1" class="d-block w-100" alt="...">
+                    <img :src="caro1" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img :src="img1" class="d-block w-100" alt="...">
+                    <img :src="caro2" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img :src="img1" class="d-block w-100" alt="...">
+                    <img :src="caro3" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img :src="caro4" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img :src="caro5" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img :src="caro6" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img :src="caro7" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img :src="caro8" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img :src="caro9" class="d-block w-100" alt="...">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -35,16 +59,32 @@
 
 <script>
 import lineupLogo from '@/assets/lineupLogo.svg'
-import img1 from '@/assets/crossimg.svg'
+import caro1 from '@/assets/caro1.svg'
+import caro2 from '@/assets/caro2.svg'
+import caro3 from '@/assets/caro3.svg'
+import caro4 from '@/assets/caro4.svg'
+import caro5 from '@/assets/caro5.svg'
+import caro6 from '@/assets/caro6.svg'
+import caro7 from '@/assets/caro7.svg'
+import caro8 from '@/assets/caro8.svg'
+import caro9 from '@/assets/caro9.svg'
 
 export default {
     name: "qWithL",
     data() {
         return {
             lineupLogo,
-            img1
+            caro1,
+            caro2,
+            caro3,
+            caro4,
+            caro5,
+            caro6,
+            caro7,
+            caro8,
+            caro9,
         }
-    }
+    },
 };
 </script>
 
