49d4131cf859079a0eed9385e20790ee03541505
Your Name <ncadaligjr@gmail.com>
2025-01-23 02:55:50 +0800

    sysadHome removed unecessary sections

--- src/views/sysadHome.vue
+++ src/views/sysadHome.vue
@@ -1,21 +1,17 @@
 <template>
     <navbar />
     <land-page />
-    <aboutUs />
-    <qWithL />
     <lineupMinds />
     <foot />
 </template>
 
 <script>
 import LandPage from '@/components/SysAd/adminlandpage.vue'
-import qWithL from '@/components/qWithL.vue'
 import navbar from '@/components/SysAd/adminnavbar.vue'
-import AboutUs from '@/components/aboutUs.vue'
 import lineupMinds from '@/components/lineupMinds.vue'
 import foot from '@/components/SysAd/adminfooter.vue'
 
 export default {
-  components: { navbar, LandPage, AboutUs, qWithL, lineupMinds, foot},
+  components: { navbar, LandPage, lineupMinds, foot},
 }
 </script>
